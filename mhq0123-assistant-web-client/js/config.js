4ef2f7f11bb5695e76f392c034b45ce4a2cb6418
mhq0123 <532386274@qq.com>
2017-01-16 17:47:50 +0800

    完善h5应用

--- mhq0123-assistant-web-client/js/config.js
+++ mhq0123-assistant-web-client/js/config.js
@@ -1,4 +1,4 @@
 var AssistantConfig = {
-	//customer_server : 'http://192.168.1.100:8099' 
-	customer_server : 'http://192.168.0.102:8099' //TODO ip不能用127.0.0.1
+	customer_server : 'http://192.168.1.117:8099' 
+//	customer_server : 'http://192.168.0.102:8099' //TODO ip不能用127.0.0.1
 }
